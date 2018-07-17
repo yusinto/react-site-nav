a4269263dabca5f8a3342abd1939f975bc81c901
Yus <yusinto@gmail.com>
2018-07-17 12:04:52 +1000

    Made nav prettier. Added sketch file.

--- examples/cra-with-nav/src/App.js
+++ examples/cra-with-nav/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import reactMenuImage from './react-logo.png';
 import aboutMeImage from './about-me.png';
-import myProjectsImage from './projects.png';
+import docsImage from './docs.png';
+import communityImage from './community.png';
+import reactSiteNavImage from './react-site-nav-logo.png';
+import tutorialImage from './tutorial.png';
 import './App.css';
 import SiteNav, {ContentGroup} from 'react-site-nav';
 
@@ -11,23 +15,37 @@ export default () => (
       <img src={logo} className="App-logo" alt="logo"/>
       <h1 className="App-title">Welcome to React</h1>
     </header>
-    <SiteNav debug={true}>
-      <ContentGroup title="About" width="240" height="140">
+    <SiteNav debug={false}>
+      <ContentGroup title="Github" width="220" height="100">
         <ul>
           <li>
-              <img src={aboutMeImage} height="40"/>
-              <span>About me</span>
+            <img src={reactMenuImage} width="30" height="30" alt="react"/>
+            <a href="https://github.com/facebook/react">React</a>
           </li>
           <li>
-            <img src={myProjectsImage} height="40"/>
-            <span>My projects</span>
+            <img src={reactSiteNavImage} height="30" alt="react site nav"/>
+            <a href="https://github.com/yusinto/react-site-nav">React Site Nav</a>
           </li>
         </ul>
       </ContentGroup>
-      <ContentGroup title="Contact" width="200" height="150">
+      <ContentGroup title="Developers" width="180" height="200">
         <ul>
-          <li><a href="https://github.com/yusinto">Github</a></li>
-          <li><a href="https://twitter.com/yusinto">Twitter</a></li>
+          <li>
+            <img src={docsImage} height="30" alt="docs"/>
+            <a href="https://reactjs.org/docs/getting-started.html">Docs</a>
+          </li>
+          <li>
+            <img src={tutorialImage} height="30" alt="tutorial"/>
+            <a href="https://reactjs.org/tutorial/tutorial.html">Tutorial</a>
+          </li>
+          <li>
+            <img src={communityImage} height="30" alt="community"/>
+            <a href="https://reactjs.org/community/support.html">Community</a>
+          </li>
+          <li>
+            <img src={aboutMeImage} height="30" alt="blog"/>
+            <a href="http://www.reactjunkie.com/">Blog</a>
+          </li>
         </ul>
       </ContentGroup>
     </SiteNav>
