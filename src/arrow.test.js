d7485069241980084877dfd06be07b7b117f3a5b
Yusinto Ngadiman <yusinto@gmail.com>
2018-08-02 19:43:29 +1000

    Added arrow.test

--- src/arrow.test.js
+++ src/arrow.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Arrow from './arrow';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
-describe('Render arrow', () => {
-  it('should render arrow correctly', () => {
-    const arrow = <Arrow/>;
+describe('<Arrow />', () => {
+  let display = 'block';
+  let fadeOut = false;
+  const fromData = {height: 270, width: 420, index: 0, left: 100};
+  const toData = {height: 270, width: 420, index: 0, left: 100};
+  const onClick = jest.fn();
+  let background = 'white';
+  let leftOffset = 0;
+  let rightOffset = 0;
 
+  const props = {
+    display,
+    fadeOut,
+    fromData,
+    toData,
+    onClick,
+    background,
+    leftOffset,
+    rightOffset,
+  };
+
+  it('should fade in on cold start', () => {
+    const arrow = renderer.create(<Arrow {...props}/>);
+    // console.log(arrow);
+    expect(arrow).toMatchSnapshot();
   });
 });
--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,4 @@
-// TODO:
-// import td from 'testdouble';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 //
-// global.td = td;
+// Enzyme.configure({adapter: new Adapter()});
